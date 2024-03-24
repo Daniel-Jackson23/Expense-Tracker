b3ffb1935fa68c092d5de0ae8e6cef7ccd0261cd
Daniel <danieljackson104@googlemail.com>
2024-03-24 22:09:01 +0000

    adding components and styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  
+  <Header />
+
+  <div class="container">
+    <Balance />
+
+    <IncomeExpense />
+    <TransactionList />
+  </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+import TransactionList from "./components/TransactionList.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpense,
+    TransactionList,
+  },
+};
+</script>
